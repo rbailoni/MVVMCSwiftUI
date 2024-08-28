ce34853298071d7702be74f67969ae4c44fe967c
Ricardo Bailoni <ricardo.bailoni@bancodaycoval.com.br>
2024-08-28 14:18:02 -0300

    Initial Commit

---
+++ NavigationStackTest/NavigationStackTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackTestApp.swift
+//  NavigationStackTest
+//
+//  Created by Ricardo Bailoni on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
