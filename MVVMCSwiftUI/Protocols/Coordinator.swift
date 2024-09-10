125088706bdf2810ac2a37abb17aa15bd22a9ec1
Ricardo Bailoni <ricardo.bailoni@bancodaycoval.com.br>
2024-09-10 17:02:37 -0300

    Adiciona Parent Coordinator

--- MVVMCSwiftUI/Protocols/Coordinator.swift
+++ MVVMCSwiftUI/Protocols/Coordinator.swift
@@ -7,8 +7,31 @@
 
 import SwiftUI
 
-protocol Coordinator {
+protocol Coordinator: AnyObject {
     var navigationPath: Binding<NavigationPath> { get }
     associatedtype V: View
     func start() -> V
+}
+
+protocol ParentCoordinator: Coordinator {
+    var childCoordinators: [any Coordinator] { get set }
+    func addChild(_ child: (any Coordinator)?)
+    func childDidFinish(_ child: (any Coordinator)?)
+}
+
+extension ParentCoordinator {
+    func addChild(_ child: (any Coordinator)?) {
+        if let child {
+            childCoordinators.append(child)
+        }
+    }
+    
+    func childDidFinish(_ child: (any Coordinator)?) {
+        for (index, coordinator) in childCoordinators.enumerated() {
+            if coordinator === child {
+                childCoordinators.remove(at: index)
+                break
+            }
+        }
+    }
 }
